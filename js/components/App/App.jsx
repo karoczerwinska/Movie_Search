ef3ee955e64d92c2212b1df700075d0ecb53b5c8
Karolina Czerwińska <karo.czerwinska@gmail.com>
2018-06-29 17:32:38 +0200

    added movie details

--- js/components/App/App.jsx
+++ js/components/App/App.jsx
@@ -8,50 +8,46 @@ import Search from '../Search/Search.jsx';
 
 export default class App extends React.Component {
 
+
     constructor(props) {
-        super(props);
+        super(props)
 
-        this.state = {
-            movieId: 12,
-
-        }
     }
 
     componentDidMount() {
 
-        let url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?&api_key=7c307736c2323ed35f5b7032e78deaea`;
-
-        this.fetchApi(url)
+        this.fetchMovieByName("nemo")
 
     }
 
-    // API
+// API
 
     fetchApi(url) {
 
-        fetch(url).then((res) => res.json()).then((data) => {
+        fetch(url)
+            .then((result) => result.json())
+            .then((data) => {
 
-            const result = data.results[0] ? data.results[0] : data;
+                const result = data.results[0] ? data.results[0] : data;
+                this.setState({
 
-            this.setState({
+                    movieID: result.id,
+                    original_title: result.original_title,
+                    tagline: result.tagline,
+                    overview: result.overview,
+                    homepage: result.homepage,
+                    poster: result.poster_path,
+                    production: result.production_companies,
+                    production_countries: result.production_countries,
+                    genre: result.genres,
+                    release: result.release_date,
+                    vote: result.vote_average,
+                    runtime_time: result.runtime,
+                    revenue: result.revenue,
+                    backdrop: result.backdrop_path
 
-                movieID: result.id,
-                original_title: result.original_title,
-                tagline: result.tagline,
-                overview: result.overview,
-                homepage: result.homepage,
-                poster: result.poster_path,
-                production: result.production_companies,
-                production_countries: result.production_countries,
-                genre: result.genres,
-                release: result.release_date,
-                vote: result.vote_average,
-                runtime: result.runtime,
-                revenue: result.revenue,
-                backdrop: result.backdrop_path
-
+                })
             })
-        })
     }
 
     fetchMovieByName = (movieName) => {
@@ -62,14 +58,84 @@ export default class App extends React.Component {
     };
 
     render() {
-        return (
-            <div>
-                <Search fetchFilm={this.fetchMovieByName}/>
-                <Card data={this.state}/>
-            </div>
-        )
+
+        if (this.state) {
+            return (
+                <div>
+                    <Search fetchFilm={this.fetchMovieByName}/>
+                    <Card data={this.state}/>
+                </div>
+            )
+        } else {
+            return null
+        }
+
+
     }
+
 }
 
 
+
+
+
+
+
+
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             movieID: 12 // pierwszy film, który sie wyswietla po załadowaniu strony
+//         }
+//     }
+//
+//     fetchApi(url) {
+//
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((data) => {
+//
+//             this.setState({
+//                 movieID: data.id,
+//                 original_title: data.original_title,
+//                 tagline: data.tagline,
+//                 overview: data.overview,
+//                 homepage: data.homepage,
+//                 poster: data.poster_path,
+//                 production: data.production_companies,
+//                 production_countries: data.production_countries,
+//                 genre: data.genres,
+//                 release: data.release_date,
+//                 vote: data.vote_average,
+//                 runtime: data.runtime,
+//                 revenue: data.revenue,
+//                 backdrop: data.backdrop_path
+//
+//             })
+//         })
+//
+//         // .catch((error) => console.log('Movie not found!'))
+//
+//     }
+//
+//     fetchMovieID = (movieID) => {
+//         let url = `https://api.themoviedb.org/3/movie/${movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+//         this.fetchApi(url)
+//     }
+//
+//     componentDidMount() {
+//         let url = `https://api.themoviedb.org/3/movie/${this.state.movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+//         this.fetchApi(url)
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <Search fetchMovieID={this.fetchMovieID}/>
+//                 <Card data={this.state}/>
+//             </div>
+//         )
+//     }
+// }
 
--- js/components/Card/Card.jsx
+++ js/components/Card/Card.jsx
@@ -6,26 +6,61 @@ let image;
 
 export default class Card extends React.Component {
 
+    componentDidMount() {
+        image = 'https://image.tmdb.org/t/p/original' + this.props.data.backdrop;
+        document.body.style.backgroundImage = 'url(' + image + ')';
+
+        let url = `https://api.themoviedb.org/3/movie/${this.props.data.movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+
+        fetch(url)
+            .then((result) => result.json())
+            .then((data) => {
+                this.setState({
+                    film: data
+                })
+            })
+    }
+
+    componentDidUpdate() {
+
+        image = 'https://image.tmdb.org/t/p/original' + this.props.data.backdrop;
+        document.body.style.backgroundImage = 'url(' + image + ')';
+
+        let url = `https://api.themoviedb.org/3/movie/${this.props.data.movieID}?&api_key=cfe422613b250f702980a3bbf9e90716`
+
+        fetch(url)
+            .then((result) => result.json())
+            .then((data) => {
+                this.setState({
+                    film: data
+                })
+            })
+    }
 
     render() {
 
-        let data = this.props.data;
+        if (!this.state) {
+            return null
+        }
+
+
+        let data = this.state.film;
+
+        console.log(data);
 
         let production = data.production;
 
-        let posterIMG = 'https://image.tmdb.org/t/p/w500' + data.poster;
+        let posterIMG = 'https://image.tmdb.org/t/p/w500' + data.poster_path;
 
         let genres = data.genre;
 
         let totalRevenue = data.revenue;
 
-        let productionList = dataToString(production);
-
-        let genresList = dataToString(genres);
+        // let productionList = dataToString(production);
+        //
+        // let genresList = dataToString(genres);
 
         let noData = '-';
-
-        image = 'https://image.tmdb.org/t/p/original' + data.backdrop;
 
 
         // jeżeli nie znaleziono ID filmu
@@ -41,11 +76,6 @@ export default class Card extends React.Component {
         } else {
             totalRevenue = data.revenue;
         }
-
-        if (data.poster == null) {
-            posterIMG = 'https://learn.getgrav.org/user/pages/11.troubleshooting/01.page-not-found/error-404.png';
-        }
-
 
         return (
             <div className="card-container">
@@ -66,18 +96,20 @@ export default class Card extends React.Component {
 
                     <div className="details">
 
-                        <span className="genre-list">{genresList}</span>
-                        <span className="production-list">{productionList}</span>
+                        {/*<span className="production-list">{productionList}</span>*/}
+
+                        {/*<span className="genre-list">{genresList}</span>*/}
+                        {/*<span className="production-list">{productionList}</span>*/}
 
                         <div className="row release-details">
 
-                            <div className="col-6"> Release Date: <span className="data">{data.release}</span></div>
+                            <div className="col-6"> Release Date: <span className="data">{data.release_date}</span></div>
 
-                            {/*<div className="col-6"> Running Time: <span className="data">{data.runtime} minutes</span>*/}
-                            {/*</div>*/}
-                            {/*<div className="col-6"> Box Office: <span className="data">${totalRevenue}</span></div>*/}
+                            <div className="col-6"> Running Time: <span className="data">{data.runtime} minutes</span>
+                            </div>
+                            <div className="col-6"> Box Office: <span className="data">${totalRevenue}</span></div>
 
-                            <div className="col-6"> Average Rating: <span className="data">{data.vote}</span></div>
+                            <div className="col-6"> Average Rating: <span className="data">{data.vote_average}</span></div>
 
                         </div>
 
@@ -90,20 +122,22 @@ export default class Card extends React.Component {
 
     }
 
-    componentDidUpdate() {
-
-        document.body.style.backgroundImage = 'url(' + image + ')';
-
-    }
 }
 
+// function dataToString(stringData) {
+//
+//     let stringArray = [];
+//
+//     stringArray.forEach(function (item, i) {
+//         stringArray.push(item.name);
+//     });
+//
+//     let resultString = console.log(stringArray.join(', ')); //
+//
+//     return resultString;
+//
+// };
 
-function dataToString(stringData) {
-    let stringArray = [];
 
-    stringArray.forEach(function (item, i) {
-        stringArray.push(item.name);
-    });
-    let resultString = stringArray.join(', '); // array to string
-    return resultString;
-};
+
+
